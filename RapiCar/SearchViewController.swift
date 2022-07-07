8d7e65a807994e7d13ee6bac53684eb808dc8c04
apachecv <apachecv@emeal.nttdata.com>
2022-07-07 11:40:08 -0500

    Agregado de Imagenes a la celda

--- RapiCar/SearchViewController.swift
+++ RapiCar/SearchViewController.swift
@@ -20,44 +20,37 @@ class SearchViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        carTableView.delegate = self
         carTableView.dataSource = self
-        carTableView.register(UINib(nibName: "Car2TableViewCell", bundle: nil), forCellReuseIdentifier: "cell2")
+        carTableView.register(UINib(nibName: "CarTableViewCell", bundle: nil), forCellReuseIdentifier: "car-cell")
+        carTableView.rowHeight = 250
         
     }
     
-    
-    
-
-    
 }
 
-extension SearchViewController: UITableViewDataSource {
+extension SearchViewController: UITableViewDataSource , UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    /*func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 100
+        }
+        else {
+            return 200
+        }
     
+        
+    
+    }*/
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as? Car2TableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "car-cell", for: indexPath) as? CarTableViewCell
         return cell!
     }
     
-    
-/*func numberOfSections(in tableView: UITableView) -> Int {
-       return 1
-    }
-    
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-return 1
-}
-
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-    let cell = tableView.dequeueReusableCell(withIdentifier: "car-cell", for: indexPath) as? CarTableViewCell
-    
-    
-    return cell!
-
-
-}*/
 }
 
